417afea76445d517e6f3d830fa901fdb60d3e6cb
maheeen-khan <maheeenkhan4@gmail.com>
2026-07-10 20:35:18 +0500

    updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,7 +14,7 @@ import DecorPage from './Pages/clientPages/DecorPage/DecorPage.jsx';
 import MakeupPage from './Pages/clientPages/MakeupPage/MakeupPage.jsx';
 import CarRentalPage from './Pages/clientPages/CarRentalPage/CarRentalPage.jsx';
 import SetupProfilePage from './Pages/clientPages/SetupProfilePage/SetupProfilePage.jsx';
-
+import BookingConfirmation from './Pages/SubPages/BookingConfirmationPage/BookingConfirmation.jsx';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -43,6 +43,9 @@ function App() {
         <Route path='/car-rental' element={<BasePage mainheading={'Wedding Car Rental in Karachi'} subhead={"Showing car services matching your budget and events. From vintage classics to modern luxury, arrive in style."}><CarRentalPage/></BasePage>}/>
 
         <Route path='/base' element={<BasePage/>}/>
+
+        <Route path='/booking-confirmation' element={<BookingConfirmation/>}/>
+
       </Routes>
     </BrowserRouter>
     </>
